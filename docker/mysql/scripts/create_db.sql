ddb6cf23a83a2199a4b97bf3279cd06a01cd1aa0
sebashack <sebastian0092@gmail.com>
2022-10-15 10:12:39 -0500

    Manually assign IP addresses mysql gateway + tweak mysql priviledge script + some refactoring

--- docker/mysql/scripts/create_db.sql
+++ docker/mysql/scripts/create_db.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE wordpressdb;
-USE wordpressdb;
-
-/* Change 127.0.0.1 to public EC2 instance IP addr */
-CREATE USER wordpress@127.0.0.1 IDENTIFIED BY 'secret';
-
-GRANT ALL PRIVILEGES
-    ON wordpressdb.*
-    TO wordpress@127.0.0.1;
+GRANT ALL ON *.* TO 'wordpress'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
