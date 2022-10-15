5d7797df93fbb0835790a9da3288a57827d6b66f
sebashack <sebastian0092@gmail.com>
2022-10-15 18:21:32 -0500

    Give ownership to ubuntu user

--- copy-wp.sh
+++ copy-wp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Only to be used inside EC2 instance with shared file system.
-cp -r wordpress /mnt/efs/wordpress-fs/
+sudo chown ubuntu:ubuntu /mnt/efs/wordpress-fs/
+if [ ! -d /mnt/efs/wordpress-fs/wordpress ]; then
+  cp -r wordpress /mnt/efs/wordpress-fs/
+fi
