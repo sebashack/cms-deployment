a92986bcf24ace2e426636f1d2c1dc4c73787f34
sebashack <sebastian0092@gmail.com>
2022-10-14 19:36:53 -0500

    php configuration take 1

--- docker/mysql/scripts/create_db.sql
+++ docker/mysql/scripts/create_db.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE wordpressdb;
 USE wordpressdb;
 
-CREATE USER wordpress@0.0.0.0 IDENTIFIED BY 'secret';
+/* Change 127.0.0.1 to public EC2 instance IP addr */
+CREATE USER wordpress@127.0.0.1 IDENTIFIED BY 'secret';
 
 GRANT ALL PRIVILEGES
-    ON wordpress.*
-    TO wordpress@0.0.0.0;
+    ON wordpressdb.*
+    TO wordpress@127.0.0.1;
 
 FLUSH PRIVILEGES;
